278d0f5781b84d2de76d7b8b2a0216623f20b9db
Mate Zoltan <mate.zoltan0@gmail.com>
2025-03-18 15:02:29 +0000

    Prepare for adding neighbour border info by using border extension with empty cells
    This makes coordinate checking unnecessary when counting neighbours.

--- dgol/cells.py
+++ dgol/cells.py
@@ -7,23 +7,32 @@ class GolCells:
         return self._cells
 
     def iterate(self) -> None:
+        self._extend_with_neighboring_border_cells()
+
         self._cells = [
             [
                 0 if (neighbors := self._neighbors(row, column)) < 2 or neighbors > 3 else
                 1 if neighbors == 3 else cell
-                for column, cell in enumerate(cell_row)
+                for column, cell in enumerate(cell_row[1:-1], start=1)
             ]
-            for row, cell_row in enumerate(self._cells)
+            for row, cell_row in enumerate(self._cells[1:-1], start=1)
         ]
 
+    def _extend_with_neighboring_border_cells(self) -> None:
+        for cell_row in self._cells:
+            cell_row.insert(0, 0)
+            cell_row.append(0)
+
+        border_row = len(self._cells[0]) * [0]
+
+        self._cells.insert(0, border_row)
+        self._cells.append(border_row)
+
     def _neighbors(self, row: int, column: int) -> int:
-        row_len = len(self._cells[0])
-        row_num = len(self._cells)
+        """Must only be called for the internal cells of the border extended cell matrix."""
 
         return sum(
             self._cells[_row][_column]
-            for _row in range(row - 1, row + 2) if 0 <= _row < row_num
-            for _column in range(column - 1, column + 2) if 0 <= _column < row_len and (
-                _column != column or _row != row
-            )
+            for _row in range(row - 1, row + 2)
+            for _column in range(column - 1, column + 2) if _column != column or _row != row
         )
